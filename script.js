aa3a2e52fad57232c1e696d076e479d42a23d93b
priyanshu1430 <priyanshugpt143@gmail.com>
2023-03-11 14:32:24 +0530

    done

---
+++ script.js
@@ -0,0 +1,47 @@
+let addButton = document.getElementById("addButton");
+let toDoContainer = document.getElementById("toContainer");
+let inputField = document.getElementById("inputField");
+let inputTime = document.getElementById("appt");
+let id = 0;
+const taks = [];
+addButton.addEventListener("click", (event) => {
+    //stops the form to submit
+    let text = document.createElement("p");
+    text.id = id;
+    text.innerHTML = `<input type="checkbox" id="check">
+        <span>
+        ${inputField.value}
+        @
+        ${inputTime.value}
+        </span>
+        <button class="del" onclick="Delete(this,${id})">delete</button>`;
+    const check = document.getElementById("check");
+    toDoContainer.appendChild(text);
+    id++;
+    inputField.value = "";
+    text.addEventListener("click", (event) => {
+        text.style.textDecoration = "line-through";
+    });
+    // text.addEventListener('dblclick',(event) =>{
+    //     toDoContainer.removeChild(text);
+    // })
+});
+function Delete(currentEl, id) {
+    const toRemove = document.getElementById(id);
+    toRemove.remove();
+    console.log(currentEl);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
